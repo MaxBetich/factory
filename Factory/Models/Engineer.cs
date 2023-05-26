a46fc698a43786d8a8f935ecf3e23cf14b7f4b74
Max Betich <betichm@gmail.com>
2023-05-26 08:28:26 -0700

    create initial project structure, add engineer, factory models

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public int EngineerId { get; set; }
+    [Required(ErrorMessage = "An engineer's name cannot be left blank")]
+    public string EngineerName { get; set; }
+    public List<EngineerMachine> JoinEntities {get;}
+  }
+}
